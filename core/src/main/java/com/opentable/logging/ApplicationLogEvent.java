4355066bda8be7c60d4cc5489aab51f12eaa9750
Kra Larivain <olarivain@gmail.com>
2018-11-12 08:18:04 -0800

    Allowing callers to override the loglov3 otl if they wish to do so (#51)
    * Allowing callers to override the loglov3 otl if they wish to do so

--- core/src/main/java/com/opentable/logging/ApplicationLogEvent.java
+++ core/src/main/java/com/opentable/logging/ApplicationLogEvent.java
@@ -99,7 +99,7 @@ class ApplicationLogEvent implements CommonLogFields
 
     @Override
     public String getLoglov3Otl() {
-        return "msg-v1";
+        return event.getMDCPropertyMap().getOrDefault("@loglov3-otl-override", "msg-v1");
     }
 
     /**
